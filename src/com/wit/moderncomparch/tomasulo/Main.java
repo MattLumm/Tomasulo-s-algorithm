19173fcb887f7a2d3743b34f51e1119e7a6e3408
leachc1 <leachc1@10.200.34.164>
2018-04-02 13:01:09 -0400

    Main class with getInput method and Boolean[] registerBusyBits

--- src/com/wit/moderncomparch/tomasulo/Main.java
+++ src/com/wit/moderncomparch/tomasulo/Main.java
@@ -1,8 +1,22 @@
 package com.wit.moderncomparch.tomasulo;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	
+    	public Boolean[] registerBusyBits;
+
+        /**
+         * Gets input for latency and reservation station size from the user in order to make the program interactive
+         * @return size - the size of the latency/reservation station
+         */
+    	public getInput() {
+    		Scanner reader = new Scanner(System.in); 
+    		System.out.println("Please enter latency/reservation station size: ");
+    		int size = reader.nextInt();
+    		return size;
+    	}
+
     }
 }
