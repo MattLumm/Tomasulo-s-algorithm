5b04421d3244eedcd2111c21e7746e8116c02d69
leachc1 <leachc1@wit.edu>
2018-04-03 12:28:28 -0400

    Separated size input into separate methods

--- src/com/wit/moderncomparch/tomasulo/Main.java
+++ src/com/wit/moderncomparch/tomasulo/Main.java
@@ -3,20 +3,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-    	
-    	public Boolean[] registerBusyBits;
+    static Scanner reader = new Scanner(System.in);
+    public Boolean[] registerBusyBits;
 
-        /**
-         * Gets input for latency and reservation station size from the user in order to make the program interactive
-         * @return size - the size of the latency/reservation station
+	public static void main(String[] args) {
+        int latency, rs;
+		getLatency();
+		getReservationStation();
+	}
+
+		/**
+         * Gets input for latency size from the user in order to make the program interactive
+         * @return size - the size of the latency
          */
-    	public getInput() {
-    		Scanner reader = new Scanner(System.in); 
-    		System.out.println("Please enter latency/reservation station size: ");
-    		int size = reader.nextInt();
-    		return size;
+    	public static int getLatency() {
+    		System.out.println("Please enter latency size: ");
+    		int latency_size = reader.nextInt();
+    		return latency_size;
     	}
 
+	/**
+	 * Gets input for reservation station size from the user in order to make the program interactive
+	 * @return size - the size of the reservation station
+	 */
+		public static int getReservationStation() {
+			Scanner reader = new Scanner(System.in);
+			System.out.println("Please enter reservation station size: ");
+			int rs_size = reader.nextInt();
+			return rs_size;
+		}
+
     }
-}
