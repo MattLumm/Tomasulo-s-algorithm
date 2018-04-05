e340c937101543101d14847756bcfc64da9e4d7e
Ford Polia <poliaf@wit.edu>
2018-04-05 12:16:01 -0400

    Removed unreacable statements

--- src/com/wit/moderncomparch/tomasulo/Issue.java
+++ src/com/wit/moderncomparch/tomasulo/Issue.java
@@ -6,22 +6,16 @@ public class Issue {
         switch (instructionType) {
             case "LDUR":
                 return Main.loadBuffer.addInstruction(instruction);
-                break;
             case "STUR":
                 return Main.storeBuffer.addInstruction(instruction);
-                break;
             case "ADDI":
                 return Main.integerReservationStation.addInstruction(instruction);
-                break;
             case "SUBI":
                 return Main.integerReservationStation.addInstruction(instruction);
-                break;
             case "FMULT":
                 return Main.floatingPointMultiplicationReservationStation.addInstruction(instruction);
-                break;
             default:
                 throw new Exception("Unknown Instruction Type: " + instructionType);
-                return false;
         }
     }
 }
